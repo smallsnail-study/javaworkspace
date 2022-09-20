ad7890d4c4c72db7fa9445a8634852bc96ec5cf4
yunhuiGwak <kwakyh17@naver.com>
2022-09-20 16:53:34 +0900

    first commit

---
+++ java/me/java/printf/Main.java
@@ -0,0 +1,15 @@
+package me.java.printf;
+
+public class Main {
+    public static void main(String[] args) {
+        int i = 10;
+        float f = 12.34f;
+        double d = 56.78;
+        char c = 'a';
+        String s = "Hello World";
+        boolean b = true;
+
+        // int
+
+    }
+}
